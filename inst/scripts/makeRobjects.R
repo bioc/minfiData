9340df3b78ac6be6b120e994dce4584dba977145
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2016-11-21 04:10:16 +0000

    updating to new SummarizedExperiment backend
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/minfiData@3992 db2202da-8704-0410-b924-b125b3cd84ef

--- inst/scripts/makeRobjects.R
+++ inst/scripts/makeRobjects.R
@@ -1,6 +1,18 @@
 library(minfi)
-sheet <- read.450k.sheet("../extdata", pattern = "csv$")
-RGsetEx <- read.450k.exp(targets = sheet)
+sheet <- read.metharray.sheet("../extdata", pattern = "csv$")
+RGsetEx <- read.metharray.exp(targets = sheet)
 save(RGsetEx, file = "../../data/RGsetEx.rda", compress = "xz")
 MsetEx <- preprocessRaw(RGsetEx)
 save(MsetEx, file = "../../data/MsetEx.rda", compress = "xz")
+
+probeTypes <- getProbeType(MsetEx, withColor = TRUE)
+keepLoci <- c(featureNames(MsetEx)[which(probeTypes == "IRed")[1:200]],
+              featureNames(MsetEx)[which(probeTypes == "IGrn")[1:200]],
+              featureNames(MsetEx)[which(probeTypes == "II")[1:200]])
+RGsetEx.sub <- subsetByLoci(RGsetEx, includeLoci = keepLoci)
+MsetEx.sub <- MsetEx[featureNames(preprocessRaw(RGsetEx.sub)),]
+save(MsetEx.sub, file = "../../data/MsetEx.sub.rda", compress = "xz")
+save(RGsetEx.sub, file = "../../data/RGsetEx.sub.rda", compress = "xz")
+sessionInfo()
+q(save = "no")
+
